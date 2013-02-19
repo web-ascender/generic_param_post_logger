05103719fd77884c9759dc4ccca994a79e975225
Ryan Doom <ryan.doom@webascender.com>
2013-02-18 23:36:11 -0500

    added horizontal rule

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,7 @@ class HomeController < ApplicationController
 
   def create
     #curl --data "payload=bomtastic&ninja=salad&pirate=smiles" http://localhost:3000/home
+    #curl --data "payload=funtimes&company=webascender&dev=ryan.doom" http://iha-generic-logger.herokuapp.com/home
     output =  params 
     output.delete(:action)
     output.delete(:controller)
