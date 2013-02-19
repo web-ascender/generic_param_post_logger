c2aaa78202a5b177ea07da9af5fbc49feadfc001
Ryan Doom <ryan.doom@webascender.com>
2013-02-19 09:21:56 -0500

    added get acceptance

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,21 @@
 class HomeController < ApplicationController
   def index
+    logit(params)
     @payloads = Payload.order("created_at DESC").limit(100)
   end
 
   def create
     #curl --data "payload=bomtastic&ninja=salad&pirate=smiles" http://localhost:3000/home
     #curl --data "payload=funtimes&company=webascender&dev=ryan.doom" http://iha-generic-logger.herokuapp.com/home
-    output =  params 
-    output.delete(:action)
-    output.delete(:controller)
-    Payload.create!(:params => output.to_yaml)
-    render :text => output
+    render :text => logit(params)    
+  end
+
+  def logit(params)
+    params.delete(:action)
+    params.delete(:controller)
+    if params.keys.count > 0
+      Payload.create!(:params => params.to_yaml)
+    end
+    return params
   end
 end
