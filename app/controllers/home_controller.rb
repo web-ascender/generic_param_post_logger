fcb9156168615cb565582cef3b7c012bc6cbab1d
Ryan Doom <ryan.doom@webascender.com>
2013-06-26 13:25:11 -0400

    mods

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,18 @@
 class HomeController < ApplicationController
   def index
     logit(params)
-    @payloads = Payload.order("created_at DESC").limit(100)
+    
   end
 
   def create
     #curl --data "payload=bomtastic&ninja=salad&pirate=smiles" http://localhost:3000/home
     #curl --data "payload=funtimes&company=webascender&dev=ryan.doom" http://iha-generic-logger.herokuapp.com/home
     render :text => logit(params)    
+  end
+
+  def show
+    #moved to the show to make it somewhat obscure
+    @payloads = Payload.order("created_at DESC").limit(300)
   end
 
   def logit(params)
